2e7d7a4c9a11e24d16c4980cda85b4ec3be4f725
claracalonge <claracalonge@hotmail.com>
2016-10-05 15:15:50 +0200

    Test de Profesor con nombre, correo, despacho.

---
+++ Horarios/test/Test/Testeo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Objetos.Profesor;
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author Clara
+ */
+public class Testeo {
+    
+    @Test
+    public void unProfesorConNombre(){
+        Profesor profesor=new Profesor("Adolfo", "a.dolfo@usp.ceu.es", "");
+        assertEquals("Adolfo",profesor.getNombre());
+        
+    }
+        @Test
+    public void unProfesorSinNombre(){
+        Profesor profesor=new Profesor(null, "a.dolfo@usp.ceu.es", "");
+        boolean sin = false;
+        if(profesor.getNombre()==null){
+            sin = true;
+        }
+        assertEquals(true,sin);
+    }
+       @Test
+    public void unProfesorConCorreo(){
+        Profesor profesor=new Profesor("Adolfo", "a.dolfo@usp.ceu.es", "");
+        assertEquals("a.dolfo@usp.ceu.es",profesor.getEmail());
+        
+    }
+       @Test
+    public void unProfesorConDespacho(){
+        Profesor profesor=new Profesor("Adolfo", "a.dolfo@usp.ceu.es", "2.2.2");
+        assertEquals("2.2.2",profesor.getDespacho());
+        
+    }
+}
