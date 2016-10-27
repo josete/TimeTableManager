6a87497a096afde122c43fedebf0082f7d1c252d
claracalonge <claracalonge@hotmail.com>
2016-10-28 01:37:39 +0200

    Predicado fuuncionando

--- No solape profesor.pl
+++ No solape profesor.pl
@@ -1,24 +1,23 @@
 %Mismo dia, a la misma hora, mismo profesor.
 
-horarioInvalido((franja(curso(N1),Asignatura1,intervalo(H1,H2),Dia),
-		franja(curso(N2),Asignatura2,intervalo(H1,H2),Dia)):-
-
-		franja(curso(N1),Asignatura1,intervalo(H1,H2),Dia),
-		franja(curso(N2),Asignatura2,intervalo(H1,H2),Dia),
-		Asignatura1\=Asignatura2,
-		profesor(Asignatura1,Prof),
-		profesor(Asignatura2,Prof)).
-
-
-
-
 mismoProfesor(profesor(Asignatura1,Nombre),profesor(Asignatura2,Nombre)):-
 	Asignatura1\=Asignatura2.
 
-franja(gisi(3),isi,intervalo(830,1030),dia(1)).
-franja(gisi(3),iac,intervalo(1030,1230),dia(1)).
-franja(gisi(3),iso,intervalo(1230,1430),dia(1)).
-franja(gisi(1),pr1,intervalo(830,1030),dia(2)).
+
+horarioInvalido(asignacion(FranjaId1, Curso1,Asignatura1,intervalo(H1,H2),Dia),
+		asignacion(FranjaId2, Curso2,Asignatura2,intervalo(H1,H2),Dia)):-
+
+		asignacion(FranjaId1, Curso1,Asignatura1,intervalo(H1,H2),Dia),
+		asignacion(FranjaId2, Curso2,Asignatura2,intervalo(H1,H2),Dia),
+		FranjaId1 \= FranjaId2,
+		profesor(Asignatura1,Prof),profesor(Asignatura2,Prof).
+
+
+asignacion(1, gisi(3),isi,intervalo(830,1030),dia(1)).
+asignacion(2, gisi(3),iac,intervalo(1030,1230),dia(1)).
+%asignacion(3, gisi(3),iac,intervalo(830,1030),dia(2)).
+asignacion(3, gisi(3),iso,intervalo(1230,1430),dia(1)).
+asignacion(4, gisi(1),pr1,intervalo(830,1030),dia(2)).
 
 profesor(iac,mfl).
 profesor(pr1,mfl).
