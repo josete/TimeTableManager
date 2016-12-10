e9f051d7595322d0ef4594b54106a79e18f41f39
josete <josecaramor1@hotmail.com!>
2016-12-10 20:56:29 +0100

    Comprobacion de asignaturas en el mismo dia

--- NodeHorarios/Source/Funcionalidad/Comprobaciones.js
+++ NodeHorarios/Source/Funcionalidad/Comprobaciones.js
@@ -37,20 +37,40 @@ var Comprobaciones = function () {
 		return puede;
 	}
 
+	this.comprobarClasesElMismoDia = function (horario, dia) {
+		mismoDia = [];
+		puede = 5;
+		Object.keys(horario).forEach(function (element) {
+			if (element.split("-")[0] == dia) {
+				mismoDia.push(element);
+			}
+		});
+
+		for (var i = 0; i < mismoDia.length; i++) {
+			for (var j = (i+1); j < mismoDia.length; j++) {
+				if (horario[mismoDia[i]].getAsignatura() == horario[mismoDia[j]].getAsignatura()) {
+					return 0;
+				}
+			}
+		}
+		return puede;
+	}
+
 
 	this.valorar = function (horario) {
-		//Valoracion del solape 10 si no hay solape y 0 si hay solape
 		var solapes = [];
 		var maximas = [];
+		var repetidas = [];
 		var self = this;
 		Object.keys(horario.horarioGenerado).forEach(function (element) {
 			try {
 				var final = element.split("-")[0] + "-" + (parseInt("10", element.split("-")[1]) + parseInt("10", horario.horarioGenerado[element].horas));
 				solapes.push(self.comprobarSolape(horario.horarioGenerado[element].getAsignatura().getProfesor(), element, final));
-				maximas.push(self.comprobarHorasDiarias(horario.horarioGenerado[element].getAsignatura().getProfesor(),horario.horarioGenerado[element],element));
+				maximas.push(self.comprobarHorasDiarias(horario.horarioGenerado[element].getAsignatura().getProfesor(), horario.horarioGenerado[element], element));
+				repetidas.push(self.comprobarClasesElMismoDia(horario.horarioGenerado, element.split("-")[0]));
 			} catch (err) { }
 		});
-		horario.valor = (Math.min.apply(Math, solapes)+Math.min.apply(Math, maximas));
+		horario.valor = (Math.min.apply(Math, solapes) + Math.min.apply(Math, maximas) + Math.min.apply(Math, repetidas));
 	}
 }
 
--- NodeHorarios/Source/Main.js
+++ NodeHorarios/Source/Main.js
@@ -9,62 +9,49 @@ var leerExcel = require("./Funcionalidad/LeerExcel.js");
 var comprobaciones = require("./Funcionalidad/Comprobaciones.js");
 var async = require("async");
 
-var Main = function() {
+var Main = function () {
 
     comprobar = new comprobaciones();
     c = new configuracion();
     leer = new leerExcel("../Info.xlsx", c);
     leer.leer();
 
-    h = new horario();
-    g = new grupo("Informatica 3", c.getCursoPorNombre("Informatica 3"));
-    h.setGrupo(g);
-    h.setSesiones(c.getSesionesPorNombreCurso("Informatica 3"));
-    h.generar();
-    c.anadirHorario(h);
-    //----------------
-    h.setSesiones(c.getSesionesPorNombreCurso("Informatica 3"));
-    h.generar();
-    c.anadirHorario(h);
-    //-------------------
-    h.setSesiones(c.getSesionesPorNombreCurso("Informatica 3"));
-    h.generar();
-    c.anadirHorario(h);
-    //h.imprimir();
-    console.log("El numero de horarios es: "+c.getHorariosDeUnGrupo("Informatica 3").length);
-    c.getHorariosDeUnGrupo("Informatica 3").forEach(function(element) {
+    for(var i=0;i<20;i++){
+        generarHorario("Informatica",3);    
+    }
+    console.log("El numero de horarios es: " + c.getHorariosDeUnGrupo("Informatica 3").length);
+    c.getHorariosDeUnGrupo("Informatica 3").forEach(function (element) {
         comprobar.valorar(element);
     });
     c.aceptarHorario("Informatica 3");
 
-    h2 = new horario();
-    g2 = new grupo("Informatica 2", c.getCursoPorNombre("Informatica 2"));
-    h2.setGrupo(g2);
-    h2.setSesiones(c.getSesionesPorNombreCurso("Informatica 2"));
-    h2.generar();
-    c.anadirHorario(h2);
-    //----------------------
-    h2.setSesiones(c.getSesionesPorNombreCurso("Informatica 2"));
-    h2.generar();
-    c.anadirHorario(h2);
-    //----------------------
-    h2.setSesiones(c.getSesionesPorNombreCurso("Informatica 2"));
-    h2.generar();
-    c.anadirHorario(h2);    
+    for(var i=0;i<20;i++){
+        generarHorario("Informatica",2);    
+    }
+    console.log("El numero de horarios es: " + c.getHorariosDeUnGrupo("Informatica 2").length);
     c.getHorariosDeUnGrupo("Informatica 2").forEach(function (element) {
         comprobar.valorar(element);
     });
     c.aceptarHorario("Informatica 2");
 
-    this.generarHorarioParaMostrar = function(numero) {
+    this.generarHorarioParaMostrar = function (numero) {
         h = c.getHorarios()[0];
         horarioEnviar = {}
-        Object.keys(h.horarioGenerado).forEach(function(element) {
+        Object.keys(h.horarioGenerado).forEach(function (element) {
             try {
                 horarioEnviar[element] = h.horarioGenerado[element].getAsignatura().getNombre();
             } catch (err) { }
         });
         return horarioEnviar;
+    }
+
+    function generarHorario(titulacion, curso) {
+        h = new horario();
+        g = new grupo((titulacion + " " + curso), c.getCursoPorNombre((titulacion + " " + curso)));
+        h.setGrupo(g);
+        h.setSesiones(c.getSesionesPorNombreCurso((titulacion + " " + curso)));
+        h.generar();
+        c.anadirHorario(h);
     }
 }
 
